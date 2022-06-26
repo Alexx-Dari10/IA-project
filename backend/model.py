9ce10c8bee486f3770ad6dd150af72025339326a
Ale <Ale@DESKTOP-KQJNTSU>
2022-06-26 13:49:04 -0400

    test

--- backend/model.py
+++ backend/model.py
@@ -7,17 +7,10 @@ generador_entrenamiento = tf.keras.preprocessing.image.ImageDataGenerator()
 datos_entrenamiento = generador_entrenamiento.flow_from_directory(os.getcwd() + "/dataset/Animals")
 
 generador_test = tf.keras.preprocessing.image.ImageDataGenerator()
+
 datos_test = generador_test.flow_from_directory(os.getcwd() + "/dataset/test", class_mode=None)
 
 lote_test = next(datos_test)
-
-from matplotlib import pyplot as plt
-plt.imshow(lote_test[0]/255.)
-plt.axis('off')
-plt.show()
-plt.imshow(lote_test[1]/255.)
-plt.axis('off')
-plt.show()
 
 inception = tf.keras.applications.InceptionV3(include_top=False, input_shape=(256, 256, 3))
 
@@ -30,7 +23,6 @@ modelo = tf.keras.models.Sequential([inception, predictor])
 modelo.compile(optimizer="adam", loss="categorical_crossentropy")
 
 modelo.fit(datos_entrenamiento, epochs=20)
-
 
 
 probs = modelo.predict(lote_test)
